44f155bf3da43832c56ac70f125bb0af48486224
Khaledhn5 <95616334+Khaledhn5@users.noreply.github.com>
2023-12-18 17:29:16 +0100

    f_song
    f_song

---
+++ src/schemas/user.py
@@ -0,0 +1,29 @@
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+
+# Schéma utilisateur de sortie (renvoyé au front)
+class UserSchema(Schema):
+    id = fields.String(description="UUID")
+    name = fields.String(description="Name")
+    
+    @staticmethod
+    def is_empty(obj):
+        return (not obj.get("id") or obj.get("id") == "") and \
+               (not obj.get("name") or obj.get("name") == "")
+
+
+class BaseUserSchema(Schema):
+    name = fields.String(description="Name")
+    password = fields.String(description="Password")
+    username = fields.String(description="Username")
+
+
+# Schéma utilisateur de modification (name, username, password)
+class UserUpdateSchema(BaseUserSchema):
+    # permet de définir dans quelles conditions le schéma est validé ou nom
+    @validates_schema
+    def validates_schemas(self, data, **kwargs):
+        if not (("name" in data and data["name"] != "") or
+                ("username" in data and data["username"] != "") or
+                ("password" in data and data["password"] != "")):
+            raise ValidationError("at least one of ['name','username','password'] must be specified")
